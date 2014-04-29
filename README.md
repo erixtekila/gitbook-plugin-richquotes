0af79cb3bedead8cc46ebe478e0b562cb2f107f4
Eric Priou <erixtekila@gmail.com>
2014-04-29 03:29:09 +0200

    Version 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-gitbook-plugin-richquotes
-=========================
+Gitbook plugin : Transform annoted quotes to notes
+==============
+
+Annotated notes are an extension of markdown blockquotes.
+Supported annotations :
+
+- > **Info** Info
+
+- > **Note** Note
+
+- > **Tag** Tag
+
+- > **Comment** Comment
+
+- > **Hint** Hint
+
+- > **Success** Success
+
+- > **Warning** Warning
+
+- > **Caution** Caution
+
+- > **Danger** Danger
+
+- > **Quote** Quote
+
+- > **Caution** Caution
+
+Lowercase and spaces are allowed for annotations.
+
+
+You can use install it via NPM:
+
+```bash
+$ npm install gitbook-plugin-richquotes
+```
+
+And use it for your book by adding `book.json`
+
+```js
+{
+	"plugins" : "richquotes"
+}
+```
+
---
+++ index.js
@@ -0,0 +1,122 @@
+var cheerio = require( "cheerio" )
+	,$
+;
+
+module.exports = {
+	book : {
+		assets: "./book",
+		css   : [
+			"plugin.css"
+		]
+	},
+	hooks: {
+		// For all the hooks, this represent the current generator
+		// This is called before the book is generated
+		init  : function ()
+		{
+			//console.log( "init!" );
+		},
+
+		// This is called after the book generation
+		finish: function ()
+		{
+			//console.log( "finish!" );
+		},
+
+		// This is called for each page of the book
+		// It can be used for modifing page content
+		// It should return the new page
+		page  : function ( page )
+		{
+			var section
+				,$bq
+				,$this
+				,$strong
+				,alert = "info"
+				,picto = "fa-info"
+				;
+			for ( var i in page.sections )
+			{
+				section = page.sections[i];
+				if ( section.type == "normal" )
+				{
+					$ = cheerio.load( page.sections[i].content );
+					$bq = $( "blockquote" );
+					if( $bq )
+					{
+						$bq.each
+						(
+							function ( j, el )
+							{
+								$this = $( this );
+								$strong = $this.find( "p > strong" );
+								if( $strong )
+								{
+									switch ( $strong.text().toLowerCase() )
+									{
+										/* info */
+										case "info" :
+											alert = "info";
+											picto = "fa-info";
+											break;
+										case "note" :
+											alert = "info";
+											picto = "fa-edit";
+											break;
+										case "tag" :
+											alert = "info";
+											picto = "fa-tag";
+											break;
+										case "comment" :
+											alert = "info";
+											picto = "fa-comment-o";
+											break;
+										/* success */
+										case "hint" :
+										case "success" :
+											alert = "success";
+											picto = "fa-lightbulb-o";
+											break;
+										/* warning */
+										case "warning" :
+										case "caution" :
+											alert = "warning";
+											picto = "fa-exclamation-triangle";
+											break;
+										/* danger */
+										case "danger" :
+											alert = "danger";
+											picto = "fa-times-circle";
+											break;
+										/* quote */
+										case "quote" :
+										case "caution" :
+											alert = "quote";
+											picto = "fa-quote-left";
+											break;
+										// Not a note
+										default :
+											return;
+									}
+
+									$strong
+										.addClass( 'fa fa-4x ' + picto )
+										.empty()
+										.remove()
+										;
+									$this.addClass( 'alert alert-' + alert );
+									$this.prepend( $strong );
+
+									// Replace by transform
+									section.content = $.html();
+								}
+							}
+						);
+					}
+				}
+			}
+
+			return page;
+		}
+	}
+};
